cf396e22248fdc8de84cf3cbbad1713b0d12f485
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2023-10-08 23:50:57 -0400

    clean up and graphs analysis

--- descent.py
+++ descent.py
@@ -7,28 +7,48 @@ STOP_LIMIT = 0.001
 def gradient(x):
     return 2 * (x-5)
 
-def next_step(current, stepSize):
-    return current - stepSize
+def next_step(current, step):
+    return current + step
 
 def main():
-    error = []
+    y_values = []
     x_values = []
+    step_values = []
+
+    #Initial plot to visualize on
     X = np.arange(10)
     Y = (X - 5) ** 2
     plt.plot(X, Y)
+   
 
-    x = 0
+    #Initial Value at random as GD is invariant to x_0
+    x = np.random.randint(0, 10)
+
+    #Standard learning rate for simple convex function
     learning_rate = 0.1
-    for i in range(TIME_STEPS):
+    for _ in range(TIME_STEPS):
+        step = learning_rate * gradient(x)
+        step_values.append(abs(step))
+
+        #Stop condition
         if abs(learning_rate * gradient(x)) < STOP_LIMIT:
             print("Learning Finished, Optimal Point : " + str(x))
-            plt.scatter(x_values, error, c='red')
+            #Scatter plot of gradient descent reaching minimum
+            plt.scatter(x_values, y_values, c='red')
+            plt.show()
+
+            #Plot of Y values, depicting the values reaching 0
+            plt.plot(y_values)
+            plt.show()
+
+            #Plot of step sizes as we get close to minimum
+            plt.plot(step_values)
             plt.show()
             return
-        x = next_step(x, learning_rate * gradient(x))
+        x = next_step(x, learning_rate * -1 * gradient(x))
         x_values.append(x)
         y_at_newx = (x - 5)**2
-        error.append(y_at_newx)
+        y_values.append(y_at_newx)
 
 if __name__ == '__main__':
     main()
